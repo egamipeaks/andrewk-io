a267e230f3e3ad0057026aabd160751f49134a00
Andrew Krzynowek <egamipeaks@gmail.com>
2025-09-01 12:06:58 -0500

    update logo

--- resources/views/emails/invoice.blade.php
+++ resources/views/emails/invoice.blade.php
@@ -5,17 +5,30 @@
 **Due Date**: {{ $invoice->due_date->format('F j, Y') }}<br>
 **Total**: {{ $invoice->formattedTotal() }}<br>
 
-<x-mail::table >
-    | Description | Rate | Total |
-    | :-----------| :---- | ----: |
-    @foreach ($invoice->invoiceLines as $line)
-        @if($line->hours > 1)
-        | {{ $line->description }} |  {{ $line->formattedSubTotal() }} x {{ $line->hours ? number_format($line->hours, fmod($line->hours, 1) ? 2 : 0) : '' }} | {{ $line->formattedSubTotal() }} |
-        @else
-            | {{ $line->description }} |  {{ $line->formattedSubTotal() }} | {{ $line->formattedSubTotal() }} |
-        @endif
-    @endforeach
-</x-mail::table>
+<table class="table" style="width: 100%; border-collapse: collapse; background: transparent;">
+    <thead>
+        <tr>
+            <th style="text-align: left; padding: 8px 0; border-bottom: 1px solid #ddd; width: 60%;">Description</th>
+            <th style="text-align: left; padding: 8px 0; border-bottom: 1px solid #ddd; width: 20%;">Rate</th>
+            <th style="text-align: right; padding: 8px 0; border-bottom: 1px solid #ddd; width: 20%;">Total</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($invoice->invoiceLines as $line)
+        <tr>
+            <td style="padding: 8px 0; border-bottom: 1px solid #f0f0f0;">{{ $line->description }}</td>
+            <td style="padding: 8px 0; border-bottom: 1px solid #f0f0f0;">
+                @if($line->hours > 1)
+                    {{ $line->formattedHourlyRate() }}<br>x {{ $line->hours ? number_format($line->hours, fmod($line->hours, 1) ? 2 : 0) : '' }}
+                @else
+                    {{ $line->formattedSubTotal() }}
+                @endif
+            </td>
+            <td style="text-align: right; padding: 8px 0; border-bottom: 1px solid #f0f0f0;">{{ $line->formattedSubTotal() }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 
 @if ($invoice->note)
 <strong>Note:</strong><br>
--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,5 +1,5 @@
 <tr>
 <td class="header">
-<img src="{{ asset('images/andrew-krzynowek-logo.png') }}" alt="andrewk.io logo" width="600" />
+<img src="{{ asset('images/logo-8bit.png') }}" alt="andrewk.io logo" width="550" />
 </td>
 </tr>
